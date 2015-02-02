5dc581b904495bb25fe3dbb8a8199ccb13b29689
Romain <rrolland.50@gmail.com>
2015-02-02 23:25:52 +0100

    player - enemy

---
+++ Assets/Script/MoveScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour {
+	
+	/// Object speed
+	public Vector2 speed = new Vector2(10, 10);
+
+	/// Moving direction
+	public Vector2 direction = new Vector2(-1, 0);
+	
+	private Vector2 movement;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		// 2 - Movement
+		movement = new Vector2(
+			speed.x * direction.x,
+			speed.y * direction.y);
+	}
+	
+	void FixedUpdate()
+	{
+		// Apply movement to the rigidbody
+		rigidbody2D.velocity = movement;
+	}
+}
---
+++ Assets/Script/PlayerScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	//Speed of ship
+	public Vector2 speed = new Vector2 (50, 50);
+	
+	private Vector2 movement;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//retrive axis info
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis ("Vertical");
+
+		//Movement per direction
+		movement = new Vector2 (
+			speed.x * inputX,
+			speed.y * inputY);
+	}
+
+	void FixedUpdate()
+	{
+		//move game object
+		rigidbody2D.velocity = movement;
+	}
+
+
+
+
+}

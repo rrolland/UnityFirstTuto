809921305aef592bf555f04e6a1d88d9e9b06fac
Romain <rrolland.50@gmail.com>
2015-02-04 22:00:26 +0100

    optimize
    spawn & camera bounds

--- Assets/Script/EnemyScript.cs
+++ Assets/Script/EnemyScript.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class EnemyScript : MonoBehaviour {
 
     private WeaponScript[] weapons;
+    private bool hasSpawn;
+    private MoveScript moveScript;
 
 	// Use this for initialization
 	void Awake() {
@@ -11,18 +13,71 @@ public class EnemyScript : MonoBehaviour {
         //retrieve weapons
         weapons = GetComponentsInChildren<WeaponScript>();
 
+        // Retrieve scripts to disable when not spawn
+        moveScript = GetComponent<MoveScript>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+    void Update()
+    {
 
-        foreach (WeaponScript weapon in weapons)
+        // 2 - Check if the enemy has spawned.
+        if (hasSpawn == false)
         {
-            //auto fire
-            if (weapon != null && weapon.CanAttack)
+            if (renderer.IsVisibleFrom(Camera.main))
             {
-                weapon.Attack(true);
+                Spawn();
             }
         }
-	}
+        else
+        {
+            foreach (WeaponScript weapon in weapons)
+            {
+                //auto fire
+                if (weapon != null && weapon.CanAttack)
+                {
+                    weapon.Attack(true);
+                }
+            }
+            // 4 - Out of the camera ? Destroy the game object.
+            if (renderer.IsVisibleFrom(Camera.main) == false)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    // 1 - Disable everything
+    void Start()
+    {
+        hasSpawn = false;
+
+        // Disable everything
+        // -- collider
+        collider2D.enabled = false;
+        // -- Moving
+        moveScript.enabled = false;
+        // -- Shooting
+        foreach (WeaponScript weapon in weapons)
+        {
+            weapon.enabled = false;
+        }
+    }
+
+    // 3 - Activate itself.
+    private void Spawn()
+    {
+        hasSpawn = true;
+
+        // Enable everything
+        // -- Collider
+        collider2D.enabled = true;
+        // -- Moving
+        moveScript.enabled = true;
+        // -- Shooting
+        foreach (WeaponScript weapon in weapons)
+        {
+            weapon.enabled = true;
+        }
+    }
 }
